bbfae437f0b0b91e2a560f48d72fbc4f6234bba1
hideonbush3 <ppoii0961@naver.com>
2023-02-15 14:38:53 +0900

    express - 개별 라우팅 처리

---
+++ express02.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const port = process.env.PORT || 3000;
+const path = require("path");
+const indexRouter = require("./routes/index.js");
+const userRouter = require("./routes/user.js");
+const aboutRouter = require("./routes/about.js");
+
+// 서버 애플리케이션 객체 생성
+const app = express();
+
+// index에 대한 route handler 지정
+app.use("/", indexRouter);
+
+app.use("/user", userRouter);
+app.use("/about", aboutRouter);
+
+app.listen(port, () => {
+  console.log("express 서버 실행중. 중지하려면 ctrl + c");
+});
