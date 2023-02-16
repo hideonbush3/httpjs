6d005aa15b7783532f9685fef0254988054c895e
hideonbush3 <ppoii0961@naver.com>
2023-02-16 17:42:09 +0900

    성적처리 1 - body-parser

--- express04.js
+++ express04.js
@@ -3,6 +3,7 @@ const userRouter = require("./routes/user.js");
 const aboutRouter = require("./routes/about.js");
 const path = require("path");
 const logger = require("morgan"); // 로그 출력기
+const bodyParser = require('body-parser');  // 폼 처리기
 
 const express = require("express");
 const port = process.env.PORT || 3000;
@@ -13,8 +14,8 @@ const app = express();
 // { 속성명 } - 구조분해할당 형식
 const { engine } = require("express-handlebars");
 
-// view 템플릿 엔진 설정
-// express 애플리케이션의 사용할 뷰 엔진을 설정하는 메서드 engine
+// view 템플릿 엔진 등록
+// express 애플리케이션의 사용할 뷰 엔진을 등록하는 메서드 engine
 // handlebars 패키지에서 가져온 engine 메서드와는 다름
 // 첫번째 매개변수는 템플릿 엔진의 이름
 // 두번째 매개변수는 템플릿 엔진의 옵션
@@ -32,8 +33,12 @@ app.engine(
     },
   })
 );
+
 app.set("views", path.join(__dirname, "views"));
-// handlebars 엔진으로 지정
+// Express 애플리케이션에서 사용할 뷰 엔진을 설정
+// hbs"는 Handlebars.js 뷰 엔진을 사용한다는 것을 의미
+// 이를 통해 express는 브라우저에 html 파일을 랜더링할때 .hbs 확장자를
+// 가진 파일을 handlebars.js 엔진을 사용하여 처리함
 app.set("view engine", "hbs");
 
 // static 폴더 안에 있는 파일을 라우팅 없이 바로 호출할 수 있도록 설정한다
@@ -45,9 +50,17 @@ app.use(express.static(path.join(__dirname, "static")));
 // 로그 설정
 app.use(logger("dev"));
 
+// 미들웨어 등록 및 설정
+app.use(express.json());
+// 전송된 폼 데이터에 대한 urlencoding 설정
+app.use(express.urlencoded({extended: false}))
+app.use(bodyParser.json())   // 전송된 폼 데이터는 json 형식
+// enctype이 text/plain 일때 필요 (비추)
+// app.use(bodyParser.text())
+
+
 // index에 대한 route handler 지정
 app.use("/", indexRouter);
-
 app.use("/user", userRouter);
 app.use("/about", aboutRouter);
 
